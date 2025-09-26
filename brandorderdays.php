03447c7656869638ea37413d627fd7ac54d64023
Lou-i3 <louis.rouault@gmail.com>
2025-09-26 17:50:28 +0200

    v1.0.2

--- brandorderdays.php
+++ brandorderdays.php
@@ -36,7 +36,7 @@ class Brandorderdays extends Module
     {
         $this->name = 'brandorderdays';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.1';
+        $this->version = '1.0.2';
         $this->author = 'dewwwe';
         $this->need_instance = 0;
 
